07d6e53ceb82c624099bb40faf29e322d6ad6c3a
iamDivin <chancedivin11@gmail.com>
2025-04-17 12:14:31 +0200

    my second commit

--- src/config/env.js
+++ src/config/env.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 export default{
     port: process.env.PORT||5000,
-    mongodburl: process.env.MONGO_URL,
+    mongodburl: process.env.MONGODB_URL,
     jwtSecret: process.env.JWT_SECRET,
     jwtexpire:process.env.JWT_EXPIRE ||'7d'
 };

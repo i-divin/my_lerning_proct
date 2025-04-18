dcb9bb90ded8ef55e192a9b83df1f5228d5a2f90
iamDivin <chancedivin11@gmail.com>
2025-04-18 12:21:32 +0200

    my second commit

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import config from "./env.js";
 
 export const connectDB =async () =>{
     try{
-        const conn=await mongoose.connect(config.mongodburl,{
+        const conn=await mongoose.connect("mongodb+srv://chancedivin11:YVQ2o4oilu5sGgy4@cluster0.oh9ngiy.mongodb.net/task_management",{
             useNewUrlparser:true,
             useUnifiedTopology:true,
         });
